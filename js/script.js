e61a2acae2248c39f8d2afb68d166555a7a9a5aa
Sangwoo Park <sangwopark19@gmail.com>
2024-05-13 13:26:41 +0900

    feat: 사용자에게 입력받은 숫자로 새 그리드를 생성하는 기능 구현 사용자 버튼을 누르면 프롬프트창을 출력하고 입력받은 숫자를 측면의 사각형 갯수로 가진 새 그리드를 생성한다

--- js/script.js
+++ js/script.js
@@ -1,20 +1,54 @@
 const container = document.querySelector(".container");
 
 function createGrid(container, count) {
-  for (let i = 0; i < count; i++) {
+  let squares = count * count;
+  for (let i = 0; i < squares; i++) {
     const div = document.createElement("div");
     div.setAttribute("class", "block");
     div.setAttribute("id", i);
+    div.setAttribute("style", `flex:1 1 ${100 / count}%`);
     // div.classList.add("pixelated");
     container.appendChild(div);
   }
+  addHoverEvent();
 }
 
-createGrid(container, 256);
-
-const allGrid = document.querySelectorAll(".block");
-allGrid.forEach((grid) => {
-  grid.addEventListener("mouseover", () => {
-    grid.classList.add("pixelated");
+function addHoverEvent() {
+  const allGrid = document.querySelectorAll(".block");
+  allGrid.forEach((grid) => {
+    grid.addEventListener("mouseover", () => {
+      grid.classList.add("pixelated");
+    });
   });
+}
+
+const padBtn = document.querySelector(".pad-btn");
+
+padBtn.addEventListener("click", () => {
+  let squaresNum;
+  while (true) {
+    squaresNum = prompt(
+      "새 그리드의 측면당 사각형 수를 알려주세요. 최대 크기는 100 입니다."
+    );
+    if (squaresNum === null) {
+      alert("취소 하셨습니다");
+      break;
+    }
+    if (squaresNum === "" || !Number.isInteger(+squaresNum)) {
+      alert("잘못 입력 하셨습니다 다시 입력해주세요.");
+      continue;
+    }
+    squaresNum = +squaresNum;
+    if (squaresNum > 100) {
+      alert("100이하로 입력해주세요.");
+      continue;
+    }
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+    createGrid(container, squaresNum);
+    break;
+  }
 });
+
+createGrid(container, 16);
